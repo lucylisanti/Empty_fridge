1efe50cbf96bf33bfde2ef4428a4e6a8d2b363ff
Lucy <lucylisanti@gmail.com>
2019-04-03 19:06:27 +0100

    Initial commit

---
+++ api_call.py
@@ -0,0 +1,13 @@
+import requests
+
+def ingredient_search(ingredient_name):
+    key = "9b6153cb33143e8ec7ab80c6c9a93eb2&"
+
+    url = 'https://www.food2fork.com/api/search?key={}q={}'.format(key, ingredient_name)
+
+    response = requests.get(url)
+
+    recipe = response.json()
+    return recipe["recipes"]
+
+
---
+++ main.py
@@ -0,0 +1,39 @@
+# main.py
+
+
+from flask import Flask, render_template, request
+
+from api_call import ingredient_search
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def recipes_site():
+#     return render_template('index.html')
+#
+# app.run(debug=True)
+
+
+# ingredient_name = input('What ingredient? ')
+#
+# recipes = ingredient_search(ingredient_name)
+#
+# pprint(recipes)
+
+
+
+@app.route('/send', methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        ingredient = request.form["ingredient"]
+
+        recipes = ingredient_search(ingredient)
+
+        return render_template("ingredient.html", ingredient=ingredient, recipes=recipes)
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
