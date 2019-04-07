ceefb7dc3562041f400f5ae45bf699d14722e005
Imo_Hetherington <hetherington.ir@gmail.com>
2019-04-07 16:13:18 +0100

    removed item outside of list

--- meat.py
+++ meat.py
@@ -1,6 +1,5 @@
 # meat
 
-Pike
 meats = ['chicken', 'lamb', 'pork', 'bacon', 'sausage', 'salami', 'prosciutto', 'jamon', 'ham', 'hotdog', 'bratwurst', 'beef', 'steak', 'brisket', 'veal', 'turkey', 'duck', 'liver', 'foie gras', 'fish', 'anchovy','basa','bass', 'bream','catfish','cod','dorade','eel','flounder','grouper','haddock','hake','halibut','herring','ilish','john dory','lamprey','lingcod','mackerel','mahi','mullet','pilchard','pollock','pomfret','pompano','salmon','sardine','shark','skate','snapper','sole','sprat','sturgeon','surimi','swordfish','tilapia','trout','tuna','turbot','whitefish','whiting']
 
 
